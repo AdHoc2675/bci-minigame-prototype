0ad714a34041941f0154d7cb6a3a77909e850f4e
AdHoc2675 <alsdnr2675@naver.com>
2024-01-22 17:32:02 +0900

    light size update

--- Assets/Scripts/AttachGameObjectsToParticles.cs
+++ Assets/Scripts/AttachGameObjectsToParticles.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 [RequireComponent(typeof(ParticleSystem))]
 public class AttachGameObjectsToParticles : MonoBehaviour
 {
     public GameObject m_Prefab;
+    private GameObject tempObj;
+    public MoucePosition moucePosition;
 
     private ParticleSystem m_ParticleSystem;
     private List<GameObject> m_Instances = new List<GameObject>();
@@ -22,9 +25,22 @@ public class AttachGameObjectsToParticles : MonoBehaviour
     {
         int count = m_ParticleSystem.GetParticles(m_Particles);
 
-        while (m_Instances.Count < count)
-            m_Instances.Add(Instantiate(m_Prefab, m_ParticleSystem.transform));
+        while (m_Instances.Count < count) { 
 
+            tempObj = Instantiate(m_Prefab, m_ParticleSystem.transform);
+
+            float temp = ((moucePosition.currentEnergy / (moucePosition.maxEnergy * 1.0f)) * 4.0f);
+
+            if (temp <= 1.0f)
+            {
+                temp = 1.0f;
+            }
+
+            tempObj.GetComponent<Light2D>().pointLightOuterRadius = tempObj.GetComponent<Light2D>().pointLightOuterRadius * temp;
+
+            //tempObj.Light = tempObj.transform.localScale * temp;
+            m_Instances.Add(Instantiate(m_Prefab, m_ParticleSystem.transform));
+        }
         bool worldSpace = (m_ParticleSystem.main.simulationSpace == ParticleSystemSimulationSpace.World);
         for (int i = 0; i < m_Instances.Count; i++)
         {
